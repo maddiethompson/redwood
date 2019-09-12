a9e5c0e7711e5b203fad428a0d6de6d4e83a804b
maddiethompson <maddie.thompson@oracle.com>
2019-09-12 13:49:29 -0500

    initial commit

---
+++ practiceApp/src/js/viewModels/incidents.js
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright (c) 2014, 2019, Oracle and/or its affiliates.
+ * The Universal Permissive License (UPL), Version 1.0
+ */
+/*
+ * Your incidents ViewModel code goes here
+ */
+
+define(['ojs/ojcore', 'knockout', 'jquery',
+    'ojs/ojknockout', 'ojs/ojmasonrylayout'],
+    function (oj, ko, $) {
+
+        function IncidentsViewModel() {
+            var self = this;
+
+
+
+
+
+
+        }
+        return new IncidentsViewModel();
+    }
+);
